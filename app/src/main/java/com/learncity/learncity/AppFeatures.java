45664816661d55904e7774b3f0c09c8c40201376
Manish <manishkumarsharma.93@gmail.com>
2016-10-13 06:54:53 +0530

    Made AppFeature an inner static class

---
+++ app/src/main/java/com/learncity/learncity/AppFeatures.java
@@ -0,0 +1,49 @@
+package com.learncity.learncity;
+
+/**
+ * Created by DJ on 10/13/2016.
+ */
+
+/**
+ * This class stores the app related features as an array of AppFeature class
+ */
+public class AppFeatures {
+
+    public static final AppFeature[] appFeatures = new AppFeature[]{
+            new AppFeature("SEARCH TUTORS", "PATH"),
+            new AppFeature("MY MATERIAL", "PATH"),
+            new AppFeature("MY PROFILE", "PATH"),
+            new AppFeature("SETTINGS", "PATH")
+    };
+    /*
+     * class encapsulating a feature/module of the app.
+     */
+    private static class AppFeature {
+
+        public String mSearchFeatureName;
+        public String mSearchFeatureIcon;
+
+        String getSearchFeatureName() {
+            return mSearchFeatureName;
+        }
+
+        void setSearchFeatureName(String mSearchFeatureName) {
+            this.mSearchFeatureName = mSearchFeatureName;
+        }
+
+        String getSearchFeatureIcon() {
+            return mSearchFeatureIcon;
+        }
+
+        void setSearchFeatureIcon(String mSearchFeatureIcon) {
+            this.mSearchFeatureIcon = mSearchFeatureIcon;
+        }
+
+        AppFeature(String searchFeatureName, String searchFeatureIcon){
+            mSearchFeatureName = searchFeatureName;
+            mSearchFeatureIcon = searchFeatureIcon;
+        }
+
+
+    }
+}
